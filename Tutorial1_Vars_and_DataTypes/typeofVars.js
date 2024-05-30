f2322e9077b56fa1bb44165e109ee7a0b5782894
Aditya <adityadudhal97@gmail.com>
2024-05-30 11:41:12 +0530

    types

---
+++ Tutorial1_Vars_and_DataTypes/typeofVars.js
@@ -0,0 +1,23 @@
+let a = 23;
+console.log('type of a')
+console.log(typeof a);
+let b = 34.56;
+console.log('type of b')
+console.log(typeof b);
+
+let x = null;
+console.log('type of x')
+console.log(typeof x);
+
+
+let y = undefined;
+console.log('type of y')
+console.log(typeof y);
+
+let aBigInt = BigInt("123");
+console.log('Value of  aBigInt')
+console.log(aBigInt);
+
+console.log('type of aBigInt')
+console.log(typeof aBigInt);
+
