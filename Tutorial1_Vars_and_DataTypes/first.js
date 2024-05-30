6f3fb53f065a66a430b5b52085696a0ea74ec57e
Aditya <adityadudhal97@gmail.com>
2024-05-30 11:15:12 +0530

    const

--- Tutorial1_Vars_and_DataTypes/first.js
+++ Tutorial1_Vars_and_DataTypes/first.js
@@ -6,7 +6,15 @@ console.log(age);
 console.log(price);
 
 //////////////////////
-let fname = 'Krishna';
-//let fname = 'Narayan'; --> This will produce error as same variable defined by let cannot be redeclared
+//Never use 'var' to declare variables
+var fAge = 89;
+var fAge = 96; //Allowed but not a good practice
+console.log(fAge);
 
-console.log(fname);
+let fName = 'Krishna';
+//let fname = 'Narayan'; //--> This will produce error as same variable defined by let cannot be redeclared
+
+const fLastName = 'Radha Krishna Yadav';
+// fLastName = 'Radha Krishna Yadavvv'; //-->This will produce error on console (ReferenceError: fname is not defined)
+
+console.log(fName);
