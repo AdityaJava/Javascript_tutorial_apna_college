3978c74769b9a73d2921d2e6dbf6389e62293a1e
Aditya <adityadudhal97@gmail.com>
2024-05-30 11:06:05 +0530

    used let

--- Tutorial1_Vars_and_DataTypes/first.js
+++ Tutorial1_Vars_and_DataTypes/first.js
@@ -4,3 +4,9 @@ price = 78.90;
 console.log(name);
 console.log(age);
 console.log(price);
+
+//////////////////////
+let fname = 'Krishna';
+//let fname = 'Narayan'; --> This will produce error as same variable defined by let cannot be redeclared
+
+console.log(fname);
