75a7ddb2550ad18bc3752b678eb0f11befc28ca0
Aditya <adityadudhal97@gmail.com>
2024-05-31 20:38:47 +0530

    replace method

--- Tutorial3_loops_and_strings/Strings/StringMethods.js
+++ Tutorial3_loops_and_strings/Strings/StringMethods.js
@@ -11,3 +11,8 @@ let str1 = 'I love Radha Krishna';
 str1[0] = 'W';  //This will not happen because strings are immutable
 
 console.log(str1);
+
+//Instead we use replace
+
+str1 = str1.replace('I', 'w');
+console.log(str1);
