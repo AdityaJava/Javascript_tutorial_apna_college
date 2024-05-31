110aa564ea3248d8b9bfd088cd676cd7485bd665
Aditya <adityadudhal97@gmail.com>
2024-05-31 11:29:49 +0530

    string started

--- Tutorial1_Vars_and_DataTypes/typeofVars.js
+++ Tutorial1_Vars_and_DataTypes/typeofVars.js
@@ -1,29 +1,29 @@
 let a = 23;
-console.log('type of a')
+console.log('type of a');
 console.log(typeof a);
 let b = 34.56;
-console.log('type of b')
+console.log('type of b');
 console.log(typeof b);
 
 let x = null;
-console.log('type of x')
+console.log('type of x');
 console.log(typeof x);
 
 
 let y = undefined;
-console.log('type of y')
+console.log('type of y');
 console.log(typeof y);
 
 let aBigInt = BigInt("123");
-console.log('Value of  aBigInt')
+console.log('Value of  aBigInt');
 console.log(aBigInt);
 
-console.log('type of aBigInt')
+console.log('type of aBigInt');
 console.log(typeof aBigInt);
 
 let aSymbol = Symbol("Hello");
-console.log('Value of  aSymbol')
+console.log('Value of  aSymbol');
 console.log(aSymbol);
 
-console.log('type of aSymbol')
+console.log('type of aSymbol');
 console.log(typeof aSymbol);
