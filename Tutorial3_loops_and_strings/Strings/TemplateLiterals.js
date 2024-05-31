a24e01fc353e0d709c4c505aa94cf1cadb03dd36
Aditya <adityadudhal97@gmail.com>
2024-05-31 20:10:36 +0530

    comment

--- Tutorial3_loops_and_strings/Strings/TemplateLiterals.js
+++ Tutorial3_loops_and_strings/Strings/TemplateLiterals.js
@@ -1,5 +1,5 @@
 //Template Literals:- The way to have embeded expressions in strings. Note this is a string only
-
+//This phenomenon is called string interpolation
 let krishnaName = 'Krishna';
 let radhaName = `radha ${krishnaName}`;
 
