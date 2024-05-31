7aad243a38d8db1853a3e19d7c57a87a88a2aa69
Aditya <adityadudhal97@gmail.com>
2024-05-31 10:01:56 +0530

    new loops

---
+++ Tutorial3_loops_and_strings/ForLoop.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//////////////For-of loop
+console.log("For-of loop----------")
+
+let aList = [1, 2, 3, 45, 6, 7, 8, 90];
+for (let u of aList) {
+    console.log(u);
+}
+
+//////////WHile Loop
+console.log("While loop----------")
+let i = 0;
+while (i < 9) {
+    console.log(i);
+    i++;
+}
