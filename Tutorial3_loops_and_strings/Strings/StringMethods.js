b98a6c6751a27fe3390e350a95899b094ad9705c
Aditya <adityadudhal97@gmail.com>
2024-05-31 20:37:08 +0530

    Strings immutable

---
+++ Tutorial3_loops_and_strings/Strings/StringMethods.js
@@ -0,0 +1,13 @@
+let str = 'Hare Krishna radhe krishna   ';
+console.log(str.toUpperCase());
+
+console.log(str.trim());
+
+
+////////Below is not possible
+
+let str1 = 'I love Radha Krishna';
+
+str1[0] = 'W';  //This will not happen because strings are immutable
+
+console.log(str1);
