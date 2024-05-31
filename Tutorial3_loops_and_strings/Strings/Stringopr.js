94018649674fc5be1d56ef3e842a43a0aa8f2b36
Aditya <adityadudhal97@gmail.com>
2024-05-31 20:02:47 +0530

    template literals

---
+++ Tutorial3_loops_and_strings/Strings/Stringopr.js
@@ -0,0 +1,11 @@
+let string = 'Radha Krishna My Love';
+
+console.log(string)
+console.log(string.length);
+
+
+console.log(string[0]);
+console.log(string[string.length - 1])
+
+///////////////////////////////
+
---
+++ Tutorial3_loops_and_strings/Strings/TemplateLiterals.js
@@ -0,0 +1,14 @@
+//Template Literals:- The way to have embeded expressions in strings. Note this is a string only
+
+let krishnaName = 'Krishna';
+let radhaName = `radha ${krishnaName}`;
+
+console.log(radhaName);
+
+///////////
+let obj = {
+    item: "pen",
+    price: 10
+}
+
+console.log(`The cost of ${obj.item} is ${obj.price}rs`);
