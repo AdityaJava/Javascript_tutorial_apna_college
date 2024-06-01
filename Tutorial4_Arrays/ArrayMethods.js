9887b9083b2fcb71ab3fa860fa325f25266c9e9e
Aditya <adityadudhal97@gmail.com>
2024-06-01 09:57:28 +0530

    push and pop

---
+++ Tutorial4_Arrays/ArrayMethods.js
@@ -0,0 +1,12 @@
+/*
+Methods of arrays push(), Pop(), toString()
+*/
+let krishnaNames = ['krishna', 'radhaVallabh', 'radhaKantKrishna', 'krish'];
+
+krishnaNames.pop();
+
+console.log(krishnaNames);
+
+krishnaNames.push('vitthal');
+console.log(krishnaNames);
+console.log(krishnaNames.toString())
