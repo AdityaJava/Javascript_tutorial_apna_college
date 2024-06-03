e08a57ea0b448d95fb1b181f6eec488cdd11727b
Aditya <adityadudhal97@gmail.com>
2024-06-03 10:39:32 +0530

    count vowel

---
+++ Tutorial5_Functions_and_Methods/practice_count_vowels.js
@@ -0,0 +1,16 @@
+function countvowels(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let vowelCount = 0;
+    for (str of string) {
+        // console.log(str);
+        if (vowels.indexOf(str) !== -1) {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
+
+let string = 'aeitgv';
+let vowelCount = countvowels(string);
+
+console.log(`Vowel count in ${string} is ${vowelCount}`);
