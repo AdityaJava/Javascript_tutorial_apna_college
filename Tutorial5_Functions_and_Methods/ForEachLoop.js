c953ded9ec7d3d8ec8e6ddca571292bb4f6d390c
Aditya <adityadudhal97@gmail.com>
2024-06-04 11:30:21 +0530

    Equivalent Java code

--- Tutorial5_Functions_and_Methods/ForEachLoop.js
+++ Tutorial5_Functions_and_Methods/ForEachLoop.js
@@ -2,6 +2,37 @@
 arr.forEach(callBackFunction)
 A callBackFunction is a function passed as an argument to another function.
 */
+/*
+Equvalent Java code// Check code in Interview project
+
+package functionalprogramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class TestFunctional {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 2, 4, 6, 7, 8);
+
+        Consumer<Integer> printVals = (Integer val) -> {
+            System.out.println(val);
+        };
+        integerList.forEach(printVals);
+
+        /////////
+        System.out.println("With Lambda Expressions");
+        integerList.forEach((Integer val) -> {
+            System.out.println(val);
+        });
+    }
+
+}
+
+
+*/
+
 
 let arr = ['a', 'b', 'c', 'd', 'e', 'f',];
 
