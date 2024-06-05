d7a1a8d77f860576addfa58b2137bf203dfb0551
Aditya <adityadudhal97@gmail.com>
2024-06-05 21:17:26 +0530

    document.querySelector

--- Tutorial6_DOM/script.js
+++ Tutorial6_DOM/script.js
@@ -1,3 +1,8 @@
 let number = prompt("Enter number to calculate it's square");
 let innerHtmlTxt = document.getElementById('result').innerHTML;
 document.getElementById('result').innerHTML = innerHtmlTxt + (number * number);
+
+///////////
+
+let innerHtmlQueryTxt = document.querySelector('#query-selector-result').innerHTML;
+document.querySelector('#query-selector-result').innerHTML = innerHtmlQueryTxt + (number * number);
