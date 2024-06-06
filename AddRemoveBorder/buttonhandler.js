2d4df20278dd04d16bd944e629e8b7552f19c296
Aditya <adityadudhal97@gmail.com>
2024-06-06 10:34:38 +0530

    Add remove border

---
+++ AddRemoveBorder/buttonhandler.js
@@ -0,0 +1,18 @@
+let isAddOn = true;
+
+function addOrRemoveBorder() {
+    console.log(isAddOn);
+    let addRemoveButton = document.querySelector('#add-remove-border');
+    if (isAddOn) {
+        console.log('in if');
+        addRemoveButton.innerHTML = 'Remove Border';
+        div.classList.add('border');
+        isAddOn = false;
+    }
+    else {
+        console.log('in else')
+        addRemoveButton.innerHTML = 'Add Border';
+        div.classList.remove('border');
+        isAddOn = true;
+    }
+}
