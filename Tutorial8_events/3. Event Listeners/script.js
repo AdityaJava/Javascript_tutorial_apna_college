6a56255b140935c8bc7aa4a1a036c49f121aeb01
Aditya <adityadudhal97@gmail.com>
2024-06-07 10:42:08 +0530

    event listeners

--- Tutorial8_events/3. Event Listeners/script.js
+++ Tutorial8_events/3. Event Listeners/script.js
@@ -1,5 +1,9 @@
 let btn1 = document.querySelector('#btn1');
 
 btn1.addEventListener('click', () => {
-    console.log('Button1 was clicked');
+    console.log('Event1 Button1 was clicked');
+});
+
+btn1.addEventListener('click', () => {
+    console.log('Event2 Button1 was clicked');
 });
