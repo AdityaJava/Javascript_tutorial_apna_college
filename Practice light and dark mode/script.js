f189b404e21844becf14cbf82e2987b29f8265cf
Aditya <adityadudhal97@gmail.com>
2024-06-07 11:37:58 +0530

    copy outside

---
+++ Practice light and dark mode/script.js
@@ -0,0 +1,17 @@
+let darkToggle = document.querySelector('#dark-selected');
+let lightToggle = document.querySelector('#light-selected');
+
+let modeButtons = document.querySelectorAll('.mode-selectors')
+
+modeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id);
+        let body = document.querySelector('body');
+        if (e.target.id === 'dark-selected') {
+            body.classList.add('darkmode');
+        }
+        else if (e.target.id === 'light-selected') {
+            body.classList.remove('darkmode');
+        }
+    });
+});
