3a8e9463d54c1cfa2c5576a00b07c8214fa4b44b
Aditya <adityadudhal97@gmail.com>
2024-06-10 19:51:59 +0530

    getting ids

---
+++ Tutorial10_stone_paper_scissors/src/script.js
@@ -0,0 +1,9 @@
+let gameImages = document.querySelectorAll('.img-tgs');
+
+console.log('event');
+console.log(gameImages);
+gameImages.forEach(gameImage => {
+    gameImage.addEventListener('click', (e) => {
+        console.log(e);
+    })
+})
