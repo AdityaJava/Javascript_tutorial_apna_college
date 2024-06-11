5deff20679103b5a05901ada5ba3dd8f11865f8c
Aditya <adityadudhal97@gmail.com>
2024-06-11 14:39:52 +0530

    randomnesss

--- Tutorial10_stone_paper_scissors/src/script.js
+++ Tutorial10_stone_paper_scissors/src/script.js
@@ -2,8 +2,24 @@ let gameImages = document.querySelectorAll('.img-tgs');
 
 console.log('event');
 console.log(gameImages);
+
+
+const generateComputerChoice = () => {
+    const options = ['rock', 'paper', 'scissors'];
+    let randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+}
+
+//Generate computer choice
+const playGame = (userChoice) => {
+    let compChoice = generateComputerChoice();
+    console.log('User choice =' + userChoice);
+    console.log('Computer choice =' + compChoice);
+}
+
 gameImages.forEach(gameImage => {
     gameImage.addEventListener('click', (e) => {
-        console.log(e.target.id);
+        let userChoice = gameImage.getAttribute('id');
+        playGame(userChoice);
     })
 })
