b0b3beb6195a6489f72a99f5891d08d9e3a6ed51
Aditya <adityadudhal97@gmail.com>
2024-06-11 15:05:17 +0530

    debugging

--- Tutorial10_stone_paper_scissors/src/script.js
+++ Tutorial10_stone_paper_scissors/src/script.js
@@ -2,12 +2,53 @@ let gameImages = document.querySelectorAll('.img-tgs');
 
 console.log('event');
 console.log(gameImages);
+const rock = 'rock';
+const paper = 'paper'
+const scissors = 'scissors';
 
 
 const generateComputerChoice = () => {
-    const options = ['rock', 'paper', 'scissors'];
+    const options = [rock, paper, scissors];
     let randomNumber = Math.floor(Math.random() * 3);
     return options[randomNumber];
+}
+
+const checkWinner = (userChoice, compChoice) => {
+    let userScore = 0;
+    let compScore = 0;
+
+    if (userChoice === compChoice) {
+        return [userScore, compScore];
+    }
+    else {
+        debugger;
+        if (userChoice === rock) {
+            if (compChoice === paper) {
+                compScore++;
+            }
+            else if (compChoice === scissors) {
+                userScore++;
+            }
+        }
+        else if (userChoice === paper) {
+            if (compChoice === rock) {
+                userScore++;
+            }
+            else if (compChoice === scissors) {
+                compScore++;
+            }
+        }
+        else if (userChoice === scissors) {
+            if (compChoice === paper) {
+                userScore++;
+            }
+            else if (compChoice === rock) {
+                compScore++;
+            }
+        }
+    }
+    return [userScore, compScore];
+
 }
 
 //Generate computer choice
@@ -15,6 +56,8 @@ const playGame = (userChoice) => {
     let compChoice = generateComputerChoice();
     console.log('User choice =' + userChoice);
     console.log('Computer choice =' + compChoice);
+    let scores = checkWinner(userChoice, compChoice);
+    console.log('Scores = ' + scores);
 }
 
 gameImages.forEach(gameImage => {
