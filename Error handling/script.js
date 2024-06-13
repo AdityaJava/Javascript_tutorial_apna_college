b9fef106a6a6864edb344724a4e6a1f4ac11f301
Aditya <adityadudhal97@gmail.com>
2024-06-13 12:05:17 +0530

    out side

---
+++ Error handling/script.js
@@ -0,0 +1,15 @@
+
+let a = 9;
+let b = 90;
+
+console.log(a + b);
+
+try {
+    //as c is not defines this will give error
+    console.log(c);
+
+} catch (error) {
+    console.log(error);
+}
+
+console.log('After error');
