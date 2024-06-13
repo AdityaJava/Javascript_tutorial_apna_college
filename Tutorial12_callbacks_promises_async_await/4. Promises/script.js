21e85b9c7296d30bae74014816fdeccf5c49db71
Aditya <adityadudhal97@gmail.com>
2024-06-13 16:28:44 +0530

    promises

--- Tutorial12_callbacks_promises_async_await/4. Promises/script.js
+++ Tutorial12_callbacks_promises_async_await/4. Promises/script.js
@@ -1,2 +1,15 @@
 //This solves callback hell problems
+//resolve and reject are callbacks provided by Javascript
+let promise = new Promise((resolve, reject) => {
+    console.log('I am a promise');
+    resolve('Promise resolved');
+});
 
+
+let promise2 = new Promise((resolve, reject) => {
+    console.log('I am promise2');
+    reject('Promise rejected');
+});
+
+//Generally we don't create promises some API returns a promise
+//But we have create a promise for book-shop-client to retrieve saved element in session storage
