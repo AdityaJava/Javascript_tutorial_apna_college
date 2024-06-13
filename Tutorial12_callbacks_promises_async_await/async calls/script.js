9d5221fa8aec8e2e58b1d092de45822cb3e3f2ba
Aditya <adityadudhal97@gmail.com>
2024-06-13 15:08:36 +0530

    async

---
+++ Tutorial12_callbacks_promises_async_await/async calls/script.js
@@ -0,0 +1,9 @@
+function hello() {
+    console.log('hello from setTimeout');
+}
+
+console.log('Hello1');
+setTimeout(hello, 4000);
+console.log('Hello2');
+console.log('Hello3');
+console.log('Hello4');
