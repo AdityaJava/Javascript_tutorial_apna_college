afd9005296e60fdd09472eea072732334f20ba42
Aditya <adityadudhal97@gmail.com>
2024-06-13 17:50:46 +0530

    async

--- Tutorial12_callbacks_promises_async_await/5. async and await/script.js
+++ Tutorial12_callbacks_promises_async_await/5. async and await/script.js
@@ -5,15 +5,17 @@ function api() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('Call is successful');
+            resolve(200);
         }, 2000);
     });
 }
 
 async function getWeatherData() {
     await api();
-    console.log('After Api call in function');
+    await api();
+    console.log('After calls in function');
 }
 
 getWeatherData();
+console.log('After calls outside function');
 
-console.log('After Api call');
