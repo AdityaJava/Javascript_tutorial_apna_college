cba99d81bcc8bbb0e64257a0a2cb8414b407a0b7
Aditya <adityadudhal97@gmail.com>
2024-06-15 12:11:08 +0530

    actual data

--- Tutorial13_Fetch_Apis/script.js
+++ Tutorial13_Fetch_Apis/script.js
@@ -5,6 +5,8 @@ const getFacts = async () => {
     console.log('Sending request');
     let response = await fetch(url);
     console.log(response);
+    let data = await response.json();
+    console.log(data);
 }
 
 getFacts();
