07ea72b9493f3bce09692dc528648b73c39037fa
Aditya <adityadudhal97@gmail.com>
2024-06-15 12:30:46 +0530

    call

--- Tutorial13_Fetch_Apis/script.js
+++ Tutorial13_Fetch_Apis/script.js
@@ -1,12 +1,17 @@
 const url = 'https://cat-fact.herokuapp.com/facts';
 
+let btn = document.querySelector('#get-facts');
+let para = document.querySelector('#cat-para');
+
 
 const getFacts = async () => {
+    console.log('btn clicked');
     console.log('Sending request');
     let response = await fetch(url);
     console.log(response);
     let data = await response.json();
     console.log(data);
+    para.innerText = data[0].text;
 }
 
-getFacts();
+btn.addEventListener('click', getFacts);
